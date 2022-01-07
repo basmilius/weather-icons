bfc38fe5ee3fdeedbfbc0ec73f369c513fbf9b97
Bas Milius <basmilius@hotmail.com>
2022-01-07 15:38:59 +0100

    feat(scripts): updated svg production file generator.

--- scripts/config.js
+++ scripts/config.js
@@ -6,18 +6,18 @@ export const STYLES_MAP = ["fill", "line", "monochrome"];
 
 export const PRODUCTION_MAP = [
     // new FontJob("fill/font", "static/export", ["svg", "ttf", "woff", "woff2"], "wi_"),
-    new PngJob("fill/png", "static/export", [64, 128, 256, 512, 1024], "wi_"),
+    // new PngJob("fill/png", "static/export", [64, 128, 256, 512, 1024], "wi_"),
     new SvgJob("fill/svg", "fill/animation-ready"),
-    new SvgJob("fill/svg-static", "static/export", "wi_"),
+    // new SvgJob("fill/svg-static", "static/export", "wi_"),
 
-    new PngJob("line/png", "line/export", [64, 128, 256, 512, 1024], "wi_"),
-    new SvgJob("line/svg", "line/animation-ready"),
-    new SvgJob("line/svg-static", "line/export", "wi_"),
+    // new PngJob("line/png", "line/export", [64, 128, 256, 512, 1024], "wi_"),
+    // new SvgJob("line/svg", "line/animation-ready"),
+    // new SvgJob("line/svg-static", "line/export", "wi_"),
 
-    new FontJob("monochrome/font", "static-monochrome/animation-ready", ["svg", "ttf", "woff", "woff2"]),
-    new PngJob("monochrome/png", "static-monochrome/export", [64, 128, 256, 512, 1024], "wi_"),
-    new SvgJob("monochrome/svg", "monochrome/animation-ready"),
-    new SvgJob("monochrome/svg-static", "monochrome/export", "wi_")
+    // new FontJob("monochrome/font", "static-monochrome/animation-ready", ["svg", "ttf", "woff", "woff2"]),
+    // new PngJob("monochrome/png", "static-monochrome/export", [64, 128, 256, 512, 1024], "wi_"),
+    // new SvgJob("monochrome/svg", "monochrome/animation-ready"),
+    // new SvgJob("monochrome/svg-static", "monochrome/export", "wi_")
 ];
 
 export async function eachJobAsync(fn) {
